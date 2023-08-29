b00283a60da91f50472d9076adbaae3d832a0b56
Raphael da Silva Ziegler <raphael@silvaziegler.com>
2023-08-29 15:32:57 +0100

    First prime finding algorithm

--- ex5-2-3/Program.cs
+++ ex5-2-3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Exercise 5.2.3
+
+using System.Diagnostics;
+
+const int maxValue = 300000;
+
+Stopwatch sw = Stopwatch.StartNew();
+List<int> primes = new List<int>() { 2 };
+
+for (int i = 3; i <= maxValue; i += 2)
+{
+    bool isPrime = true;
+
+    foreach (var j in primes)
+    {
+        if (i / j * j == i)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime)
+    {
+        primes.Add(i);
+        
+    }
+}
+
+Console.WriteLine($"Took {sw.ElapsedMilliseconds:N0} ms");
+
+
